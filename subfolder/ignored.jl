6a22bc07d0e7e506d5eba94d0ca5c3551ca2da0f
Connor Burns <ctrekker4@gmail.com>
2023-07-03 10:42:38 -0600

    Add ignored notebooks

---
+++ subfolder/ignored.jl
@@ -0,0 +1,55 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4d19597d-60fe-40d9-9b6a-859c26924d18
+using Dates
+
+# ╔═╡ 13c2d8aa-19c0-11ee-1845-7b034324f92d
+md"""
+# Ignored Notebook
+"""
+
+# ╔═╡ a84c4ee3-f773-42bc-9bd4-83fed4d39c97
+md"""
+This notebook should __NOT__ be exported since it is in `.glassignore`
+"""
+
+# ╔═╡ dc520336-aefc-4595-b065-31925a4acd12
+now()
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+Dates = "ade2ca70-3891-5945-98fb-dc099432e06a"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.9.0"
+manifest_format = "2.0"
+project_hash = "d7cd76e304b32b583eb96a7ac19153dc0f2d1730"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+"""
+
+# ╔═╡ Cell order:
+# ╟─13c2d8aa-19c0-11ee-1845-7b034324f92d
+# ╟─a84c4ee3-f773-42bc-9bd4-83fed4d39c97
+# ╠═4d19597d-60fe-40d9-9b6a-859c26924d18
+# ╠═dc520336-aefc-4595-b065-31925a4acd12
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
