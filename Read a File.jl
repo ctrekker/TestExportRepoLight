f7ce24ff67fd46e90a9fbad1170735853f42e6b3
Connor Burns <cburns@cot.llc>
2024-01-07 01:21:42 +0000

    Update notebook again

--- Read a File.jl
+++ Read a File.jl
@@ -14,6 +14,9 @@ open("test.txt", "r") do io
 	read(io, String)
 end
 
+# ╔═╡ 7b99dddf-ef7c-4580-9a0c-d13dcc6b8ea0
+
+
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
@@ -33,5 +36,6 @@ project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 # ╔═╡ Cell order:
 # ╟─c860ff61-d08e-4af0-be08-34d4d0652437
 # ╠═e9fedaa0-638f-11ed-2bbd-33017c2e967c
+# ╟─7b99dddf-ef7c-4580-9a0c-d13dcc6b8ea0
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
