209cf2be58c96f576f4eb28e3dbce031e9705deb
Connor Burns <cburns@cot.llc>
2024-03-02 22:57:39 +0000

    Add 404 notebook

---
+++ 404.jl
@@ -0,0 +1,17 @@
+### A Pluto.jl notebook ###
+# v0.19.40
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 18ff75ae-d8e8-11ee-3874-c5afc5c1b60b
+md"""
+# My Custom 404 Page :)
+"""
+
+# ╔═╡ bdd5e033-98fa-4d7f-8b47-13256579057b
+versioninfo()
+
+# ╔═╡ Cell order:
+# ╟─18ff75ae-d8e8-11ee-3874-c5afc5c1b60b
+# ╠═bdd5e033-98fa-4d7f-8b47-13256579057b
